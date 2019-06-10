f4b10a0b5b2630fd58614dd49082cfcaf46d741f
Matthew Peoples <matthew.peoples@yale.edu>
2019-06-10 16:29:40 -0400

    still working

--- lib/menu.rb
+++ lib/menu.rb
@@ -3,14 +3,14 @@ def clear_screen
 end
 
 def start_game
-  # Catpix::print_image "./media/planet_pic.jpg",
-  #   :limit_x => 0.5,
-  #   :limit_y => 0.5,
-  #   :center_x => false,
-  #   :center_y => false,
-  #   :resolution => "auto"
-  #
-  # puts "\n\n\n\n\n"
+  Catpix::print_image "./media/planet_pic.jpg",
+    :limit_x => 0.5,
+    :limit_y => 0.5,
+    :center_x => false,
+    :center_y => false,
+    :resolution => "auto"
+
+  puts "\n\n\n"
 
   begin_this_game
 end
@@ -26,7 +26,24 @@ def begin_this_game
   if User.all.find_by(name: name_input)
     active_user =  User.all.find_by(name: name_input)
     clear_screen
-    puts "Welcome back Almighty #{name_input}"
+    puts "Welcome back Almighty #{active_user.name}"
+  else
+    new_user = User.create(name: name_input)
+    user_id = new_user.id
+    puts "Welcome, Almighy #{new_user.name}"
+    ask_for_galaxy(user_id)
+  end
+end
+
+def ask_for_galaxy(id_use)
+  prompt = TTY::Prompt.new
+
+  galaxy_input = prompt.ask("My liege, what will you call your galaxy?") do |q|
+    q.required true
+    q.convert :string
   end
 
+  new_galaxy = Galaxy.create(name: galaxy_input, user_id: id_use)
+  puts "Enjoy your new galaxy, #{new_galaxy.name}"
+  User.find(id_use).galaxies << new_galaxy
 end
